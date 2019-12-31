d7e3ec40a40be840ca5e87e28758c5dcf88ebc1a
Rickard Andersson <gonz@severnatazvezda.com>
2019-12-31 13:14:55 +0200

    add `map`

--- src/slice_utilities.zig
+++ src/slice_utilities.zig
@@ -3,8 +3,8 @@ const mem = std.mem;
 const testing = std.testing;
 
 /// Filters a slice of `T` and puts the result into a new allocated and resized slice.
-/// The caller is responsible for freeing the associated memory.
-pub fn filterSlice(
+/// The caller is responsible for freeing the allocated memory.
+pub fn filter(
     comptime T: type,
     allocator: *mem.Allocator,
     slice: []const T,
@@ -23,12 +23,44 @@ pub fn filterSlice(
     return filtered;
 }
 
-test "`filterSlice` works" {
+/// Maps a function `f` over a slice of `A`s to create a slice of `B`s. `A` and `B` may or may not
+/// be the same type; the only requirement is that they match the type of `f`.
+/// The caller is responsible for freeing the allocated memory.
+pub fn map(
+    comptime A: type,
+    comptime B: type,
+    allocator: *mem.Allocator,
+    as: []const A,
+    f: fn (x: A) B,
+) ![]B {
+    var bs = try allocator.alloc(B, as.len);
+
+    for (as) |a, i| {
+        bs[i] = f(a);
+    }
+
+    return bs;
+}
+
+test "`filter` works" {
     const slice = &[_]u32{ 1, 2, 3, 4, 5 };
-    const filtered = try filterSlice(u32, std.heap.page_allocator, slice, isEven);
+    const filtered = try filter(u32, std.heap.page_allocator, slice, isEven);
     testing.expectEqualSlices(u32, filtered, &[_]u32{ 2, 4 });
+}
+
+test "`map` works" {
+    const slice = &[_]u32{ 1, 2, 3, 4, 5 };
+    const mappedPlusOne = try map(u32, u32, std.heap.page_allocator, slice, addOne);
+    const mappedIsEven = try map(u32, bool, std.heap.page_allocator, slice, isEven);
+
+    testing.expectEqualSlices(u32, mappedPlusOne, &[_]u32{ 2, 3, 4, 5, 6 });
+    testing.expectEqualSlices(bool, mappedIsEven, &[_]bool{ false, true, false, true, false });
 }
 
 fn isEven(x: u32) bool {
     return x % 2 == 0;
+}
+
+fn addOne(x: u32) u32 {
+    return x + 1;
 }
